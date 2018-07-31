6589dfb32a121b91db410f4392f5373ea6e4c0c6
Jay <paxformatus@gmail.com>
2018-07-31 01:15:28 -0400

    JS commit 2

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,10 @@
+//SteamID64 keys, to be grabbed from DOM input
 var steamID1 = '76561197967234047';
 var steamID2 = '';
 
 function playerOneAjax(){
 
+  //First player call to API for game list
 var queryURL = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=5EB85FE3C64B2FECF3B4CBE03147F65E&steamid="+steamID1+"&format=json";
         
     $.ajax({
@@ -15,6 +17,7 @@ var queryURL = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?
 
         }
 
+  //Second player call to API for game list
 function playerTwoAjax(){
 
 var queryURL = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=5EB85FE3C64B2FECF3B4CBE03147F65E&steamid="+steamID2+"&format=json";
