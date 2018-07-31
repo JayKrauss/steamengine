ff49dd15db214dccbe966dc2135b5327a1c8b299
Jay <paxformatus@gmail.com>
2018-07-31 15:23:23 -0400

    broken api commit

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,11 +4,19 @@ var steamID;
 //appID, to be generated via the player API call
 var appID;
 
+//variable to hold player being called
+var player;
+
+//Array to hold games from API
+var gamesArray = [];
+
 //First player call to API for game list
 function playerAjax(){
-  
+
 var queryURL = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=5EB85FE3C64B2FECF3B4CBE03147F65E&steamid=${steamID}&format=json`;
-        
+
+var games;
+
     $.ajax({
           url: queryURL,
           method: "GET"
@@ -16,9 +24,23 @@ var queryURL = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?
           .then(function(response) {
             console.log(queryURL);
             console.log(response);
-          });
+            games = response.games
+            // parseGames(games);
+      
+
+        });
+      }
+  
+function parseGames(games) {
+    for (i = 0; i < games.length; i++) {
+        for (y = 0; y < gameIds[i].games.length; y++) {
+            gamesArray.push(games[i].appID)
+            // gameIds[1].games[5].id);
 
         }
+    }
+    console.log(gamesArray);
+};
 
   //Calls to API to pull the game name from the APPID provided by the JSON object
 function gameCall(){
@@ -32,7 +54,7 @@ var queryURLGame = `http://api.steampowered.com/ISteamUserStats/GetSchemaForGame
           .then(function(response) {
             console.log(response.game.gameName);
             console.log(response);
-            $('#infocurrent1').append(response.game.gameName)
+            return(response.game.gameName)
           });
 
         }
@@ -46,10 +68,11 @@ $('#infocurrent1').html(`<img src="https://steamcdn-a.opskins.media/steam/apps/$
 
   //doc.ready to allow for DOM input
 $(document).ready(function(){
-  
+
   //Left submit button functionality
   $('#p1Btn').on('click', function(){
     event.preventDefault();
+    player = 1;
     steamID = $('#steamID1').val().trim();
     console.log(steamID);   
     playerAjax();
@@ -58,6 +81,7 @@ $(document).ready(function(){
   //Right submit button functionality
   $('#p2Btn').on('click', function(){
     event.preventDefault();
+    player = 2;
     steamID = $('#steamID2').val().trim();
     console.log(steamID);   
     playerAjax();
